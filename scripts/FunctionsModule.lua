82ba6e628a29818d852caa66daab06dee71ef606
m79054050200 <m79054050200@gmail.com>
2020-10-17 18:47:35 +0300

    Добавил функцию для добавления фрейму кнопки действия при нажатии на нее

--- scripts/FunctionsModule.lua
+++ scripts/FunctionsModule.lua
@@ -1,3 +1,10 @@
 WM("FunctionsModule", function(import, export, exportDefault)
     local GAMEUI = import("GAMEUI", "OriginFramesModule")
+
+    -- Принимает два параметра: Кнопку и действие
+    export("ButtonAddAction", function(buttonFrame, buttonClickAction)
+        local buttonEventTrigger = CreateTrigger()
+        TriggerAddAction(buttonEventTrigger, buttonClickAction)
+        BlzTriggerRegisterFrameEvent(buttonEventTrigger, buttonFrame, FRAMEEVENT_CONTROL_CLICK)
+    end)
 end)
