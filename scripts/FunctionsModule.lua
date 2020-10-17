e580d9a2bf58f15d90556512dcb6044ebc1a3761
m79054050200 <m79054050200@gmail.com>
2020-10-17 18:49:04 +0300

    Добавил функцию, которая создает кнопку

--- scripts/FunctionsModule.lua
+++ scripts/FunctionsModule.lua
@@ -7,4 +7,17 @@ WM("FunctionsModule", function(import, export, exportDefault)
         TriggerAddAction(buttonEventTrigger, buttonClickAction)
         BlzTriggerRegisterFrameEvent(buttonEventTrigger, buttonFrame, FRAMEEVENT_CONTROL_CLICK)
     end)
+
+    -- Принимает шесть параметров: Название кнопки, путь к изображению иконки
+    -- расположение кнопки по x, расположение кнопки по y, ширина кнопки, высота кнопки
+    -- Возвращает созданную кнопку
+    export("CreateButton", function(buttonName, iconPath, x, y, width, height)
+        local ButtonFrame = BlzCreateFrameByType("GLUEBUTTON", buttonName, GAMEUI, "ScoreScreenTabButtonTemplate", 0)
+        local ButtonIconFrame = BlzCreateFrameByType("BACKDROP", (buttonName.."Icon"), ButtonFrame, "", 0)
+        BlzFrameSetAllPoints(ButtonIconFrame, ButtonFrame)
+        BlzFrameSetTexture(ButtonIconFrame, iconPath, 0, true)
+        BlzFrameSetAbsPoint(ButtonFrame, FRAMEPOINT_BOTTOM, x, y)
+        BlzFrameSetSize(ButtonFrame, width, height)
+        return ButtonFrame
+    end)
 end)
