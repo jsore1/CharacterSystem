b5101b28c4a20f4a508ae047e21a9c8dc2c2d696
m79054050200 <m79054050200@gmail.com>
2020-10-18 20:28:06 +0300

    Изменил код для тестирования мультиплеера

--- scripts/MainModule.lua
+++ scripts/MainModule.lua
@@ -6,39 +6,45 @@ WM("MainModule", function(import, export, exportDefault)
     -- Функция для добавления кнопке события
     local buttonAddAction = import("ButtonAddAction", "FunctionsModule")
     
-    local x, y = GetPlayerStartLocationX(GetLocalPlayer()), GetPlayerStartLocationY(GetLocalPlayer())
-    local hero = CreateUnit(GetLocalPlayer(), FourCC("Hblm"), x, y, 270)
-    SelectUnit(hero, true)
-    local HeroCharacter = Character:new({ unit = hero, player = GetLocalPlayer() })
+    local x = {}
+    local y = {}
+    x[1], y[1] = GetPlayerStartLocationX(Player(0)), GetPlayerStartLocationY(Player(0))
+    x[2], y[2] = GetPlayerStartLocationX(Player(1)), GetPlayerStartLocationY(Player(1))
+
+    local hero = {}
+    hero[1] = CreateUnit(Player(0), FourCC("Hblm"), x[1], y[1], 270)
+    hero[2] = CreateUnit(Player(1), FourCC("Hblm"), x[2], y[2], 270)
+    
+    local HeroCharacter = {}
+    HeroCharacter[1] = Character:new({ unit = hero[1], player = Player(0) })
+    HeroCharacter[2] = Character:new({ unit = hero[2], player = Player(1) })
 
     -- Создаем кнопку инвентаря
     local inventoryButtonFrame = createButton("InventoryButton", "ReplaceableTextures\\CommandButtons\\BTNDust.blp", 0.215, 0.14, 0.024, 0.024)
     -- Добавляем кнопке инвентаря событие нажатия
     buttonAddAction(inventoryButtonFrame, function()
-        local visible = BlzFrameIsVisible(HeroCharacter.inventoryFrame[1])
-        BlzFrameSetVisible(HeroCharacter.inventoryFrame[1], not visible)
-        BlzFrameSetVisible(HeroCharacter.inventoryFrame[2], not visible)
-        --print("InventoryButton Clicked")
-        if GetLocalPlayer() == GetTriggerPlayer() then
-            BlzFrameSetEnable(BlzGetTriggerFrame(), false)
-            BlzFrameSetEnable(BlzGetTriggerFrame(), true)
+        for i = 1, #HeroCharacter do
+            if GetLocalPlayer() == GetTriggerPlayer() and HeroCharacter[i].player == GetTriggerPlayer() then
+                local visible = BlzFrameIsVisible(HeroCharacter[i].inventoryFrame[1])
+                BlzFrameSetVisible(HeroCharacter[i].inventoryFrame[1], not visible)
+                BlzFrameSetVisible(HeroCharacter[i].inventoryFrame[2], not visible)
+                --print("InventoryButton Clicked")
+                BlzFrameSetEnable(BlzGetTriggerFrame(), false)
+                BlzFrameSetEnable(BlzGetTriggerFrame(), true)
+                print(GetTriggerPlayer())
+            end
         end
     end)
 
-    local tr = CreateTrigger()
-    TriggerRegisterUnitEvent(tr, hero, EVENT_UNIT_PICKUP_ITEM)
-    TriggerAddAction(tr, function ()
-        --print(BlzGetItemTooltip(GetManipulatedItem()))
-        HeroCharacter:addItem(GetManipulatedItem())
-    end)
-
-    local tr1 = CreateTrigger()
-    TriggerRegisterPlayerChatEvent(tr1, GetLocalPlayer(), "test", true)
-    TriggerAddAction(tr1, function ()
-        HeroCharacter:putOn(1, 1)
-        --BlzFrameSetVisible(HeroCharacter.inventoryFrame[1], false)
-        --BlzFrameSetVisible(HeroCharacter.inventoryFrame[2], false)
-    end)
+    local triggerUnitPickupItem = {}
+    for i = 1, #HeroCharacter do
+        triggerUnitPickupItem[i] = CreateTrigger()
+        TriggerRegisterUnitEvent(triggerUnitPickupItem[i], hero[i], EVENT_UNIT_PICKUP_ITEM)
+        TriggerAddAction(triggerUnitPickupItem[i], function ()
+            --print(BlzGetItemTooltip(GetManipulatedItem()))
+            HeroCharacter[i]:addItem(GetManipulatedItem())
+        end)
+    end
 
     print("MainModule загружен")
 end)
